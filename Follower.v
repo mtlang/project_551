ec235c51c6f725f50be775e6b24088af115de871
tylerreedyoung <tryoung@wisc.edu>
2017-04-29 12:12:18 -0500

    Add files via upload
    The top level module that houses the modules we have already created

---
+++ Follower.v
@@ -0,0 +1,4 @@
+module (.clk(clk),.RST_n(rst_n),.led(led),.a2d_SS_n(a2d_SS_n),
+              .SCLK(SCLK),.MISO(MISO),.MOSI(MOSI),.rev_rht(rev_rht),.rev_lft(rev_lft),.fwd_rht(fwd_rht),
+			  .fwd_lft(fwd_lft),.IR_in_en(IR_in_en),.IR_mid_en(IR_mid_en),.IR_out_en(IR_out_en),
+			  .in_transit(in_transit),.OK2Move(OK2Move),.buzz(buzz),.buzz_n(buzz_n),.RX(RX),.BC(BC))
