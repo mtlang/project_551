07fbd195a29514dbef7dcb6be9eb813efde77b74
mtlang <mtlang@comcast.net>
2017-05-08 14:04:35 -0500

    Created folder of files for final submission

---
+++ Final files for Submission/files for project.zip/SPI_mstr.sv
@@ -0,0 +1,178 @@
+module SPI_mstr(rd_data, done, SS_n, MOSI, SCLK, wrt, cmd, MISO, clk, rst_n);
+
+// states
+typedef enum reg [1:0]{IDLE, START, BACK_PORCH} state_t;
+
+state_t state, nxt_state;
+
+output [15:0] rd_data;			// data packet from slave
+output  reg done,				// transaction complete
+       	   SS_n,				// slave select 1 bit
+           MOSI, 				// MASTER OUTPUT SLAVE INPUT
+           SCLK;				// operating clk for transaction driven by master
+       
+input wrt,				// signal to load cmd to shift reg
+      clk,				// System clock
+      rst_n,				// async reset
+      MISO;				// MASTER INPUT SLAVE OUTPUT
+
+input [15:0] cmd;			// cmd to be loaded to shift register and sent to SLAVE
+wire [15:0] next_shift_reg;
+reg [15:0] shift_reg;			// 16 bit shift register for SPI master
+reg shift;				// shift register should shift two system clocks after the rise of SCLK
+wire [4:0] next_counter;
+wire [5:0] next_cycles;
+reg inc_dcount;
+reg clr_dcnt;
+reg [4:0] counter; 			// 5 bit counter for mod 5 (32 divison)
+reg [5:0]  cycles; 			// counts how many shifts have occured
+reg [1:0] dcount;
+
+reg enable,				// enable the counter
+    clr_cnt;				// clear both counter and cycles
+
+reg SCLK_FF1, SCLK_FF2, SCLK_FF3;	// system clock flip flops for edge detection
+reg sendMOSI, h_SS_n;			// sends MOSI to slave, detects if SS_n is high for two cycles
+
+reg wrt_again;				// writes again in order to sync signals
+wire [1:0] next_dcount; 		// feeds the dcount flip flop
+
+assign next_dcount = inc_dcount ? dcount + 1'b1 :
+			clr_dcnt ? 2'b00 : dcount;
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		dcount <= 2'b00;
+	else 
+		dcount <= next_dcount;
+end
+
+// sequential state logic
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		state <= IDLE;		// default state is IDLE
+	else
+		state <= nxt_state;
+end
+
+// Main shift register
+assign next_shift_reg = wrt||wrt_again ? cmd : 
+			shift ? {shift_reg[14:0], MISO} : shift_reg;
+
+always@(posedge clk or negedge rst_n) begin
+	if(~rst_n)
+		shift_reg <= 16'h0000;
+	else 
+		shift_reg <= next_shift_reg;	// when shift is asserted shift MSB out and put MISO in LSB
+end
+
+
+// sequential counter for mod 32 division and cycle to keep track of how many bits has been shifted
+assign next_counter = clr_cnt ? 5'b11110 : 
+		      (enable && counter == 5'b11111) ? 5'b00000: 
+		      enable ? (counter + 5'b00001) : counter;
+
+assign next_cycles = clr_cnt ? 6'b000000 : 
+		     (enable && counter == 5'b11111) ? (cycles + 6'b000001) : cycles;
+
+always@(posedge clk or negedge rst_n) begin
+ if(~rst_n)
+	counter <= 5'b00000;
+ else 
+	counter <= next_counter;// increment counter to get SCLK 
+//might need reset logic in these 
+
+end
+
+always@(posedge clk or negedge rst_n) begin
+ if(!rst_n)
+	cycles <= 6'b000000;
+ else
+	cycles <= next_cycles; // increment counter to get SCLK
+
+end
+
+
+// sequential logic for SCLK for edge detection and when to assert various signals
+always@(posedge clk or negedge rst_n) begin
+  if (~rst_n) begin
+      SCLK_FF1 <= 1;
+      SCLK_FF2 <= 1;
+      SCLK_FF3 <= 1;
+end
+   else begin
+      SCLK_FF1 <= SCLK;
+      SCLK_FF2 <= SCLK_FF1;
+      SCLK_FF3 <= SCLK_FF2;
+end
+	
+end
+
+assign sendMOSI = (SCLK_FF1 & SCLK_FF1) & ~SCLK_FF3;	// Send MOSI to slave if SCLK is 0,1,1 in the past 3 cycles (in that sequence) so 2 clks after SCLK went high
+assign h_SS_n = SCLK_FF1 & SCLK_FF2;			// set SS_n high during last state if past SCLK was 1,1 (means two cycle has passed and can send SS_n high
+assign SCLK = enable ? counter[4] : 1;			// SCLK is based of 4th bit of counter when enable is high, otherwise it is high
+assign shift = (SCLK_FF1 & ~SCLK_FF2) ? 1 : 0;		// shift occurs at rising edge of SCLK same as was 0, 1.
+assign rd_data = done ? shift_reg : rd_data;
+assign MOSI = sendMOSI ? shift_reg[15] : MOSI;
+
+
+// combinational logic
+always_comb begin
+// default values
+done = 1'b0;
+SS_n = 1'b1;
+nxt_state = IDLE;
+clr_cnt = 1'b0;
+enable = 1'b0;
+inc_dcount = 1'b0;
+clr_dcnt = 1'b0;
+wrt_again = 0;
+
+
+case (state)
+IDLE: begin
+  if (wrt) begin			// initiate MASTER SPI WRITE
+      SS_n = 1'b0;
+      nxt_state = START;
+      clr_cnt = 1'b1;			// clears count and cycle so process starts smoothly
+      clr_dcnt = 1'b1;
+  end
+end
+
+START: begin
+SS_n = 1'b0;				// SS_n is pulled low for whole SPI transaction
+  if (sendMOSI) begin
+      enable = 1'b1;
+      nxt_state = START;
+  end else if (cycles == 6'b010001) begin		// if cycle is 17, then transaction is complete proceed to last state where it makes sure SCLK is high before SS_n is high
+      nxt_state = BACK_PORCH;
+  end
+  else begin
+      enable = 1'b1;
+      nxt_state = START;
+  end
+end
+
+BACK_PORCH: begin
+  if (h_SS_n && dcount == 1) begin			// When SCLK has been high for 2 consecutive clock cycles then it will pull up SS_n and flag device that transfer is complete
+	done = 1'b1;
+  end 
+  else if (dcount != 1) begin
+      inc_dcount = 1'b1;
+      SS_n = 1'b0;
+      nxt_state = START;
+      clr_cnt = 1;
+      wrt_again = 1;
+end
+  else begin
+      SS_n = 1'b0;
+      nxt_state = BACK_PORCH;
+  end
+end
+
+default:  // default state
+  nxt_state = IDLE;
+endcase
+
+end
+endmodule
+
---
+++ Final files for Submission/files for project.zip/UART_rcv.sv
@@ -0,0 +1,160 @@
+module uart_rcv(rx_rdy, rx_data, clk, rst_n, RX, clr_rx_rdy);
+
+typedef enum reg [1:0] {IDLE, START, RECEIVING} state_t;	// state names
+state_t state,		// current state
+	nxt_state;	// next state
+
+localparam BAUD = 2604;	// clk cycle per baud cycle [duration = BAUD*10 = 26,040 clks]
+
+output reg rx_rdy;	// Asserted when a whole byte is received. Deasserted with rx_rdy_clr|| new byte
+output reg [7:0] rx_data;	// Byte received
+
+input clk,		// 50MHz system clock
+      rst_n,		// Active low async reset
+      RX,		// Serial data input
+      clr_rx_rdy;	// Asserted to clear rx_rdy
+
+reg RX_FF1, RX_FF2;	// flop for falling edge detection
+
+reg falling_edge_rx,	// 1 if RX is falling, 0 otherwise
+     start,		// 1 to start receving, 0 otherwise
+     done,		// 1 if all 10 bits received, 0 otherwise 
+     shift;		// shift occurs at 1/2 baud, then every 1 baud after for 9 cycles
+
+reg [11:0] baud_cnt;	// baud counter for when to sample
+reg [9:0] shift_reg;	// RX data is shifted into this reg
+reg [4:0] cycle_cnt;	// keeps track of cycles, done after 10 cycles
+reg inc_cycle;
+wire next_rx_rdy;	// feeds the rx_rdy flip flop
+wire [11:0] next_baud_cnt;	// feeds the baud_cnt flip flop
+wire [9:0] next_shift_reg;	// feeds the shift_reg flip flop
+wire [4:0] next_cycle_cnt;	// feeds the cycle_cnt flip flop
+
+// rx_rdy FF 
+assign next_rx_rdy = (clr_rx_rdy | start) ? 1'b0 : 
+			done ? 1'b1 : rx_rdy;
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		rx_rdy <= 1'b0;
+	else if (clr_rx_rdy | start | done)
+		rx_rdy <= next_rx_rdy;
+end
+
+
+// state machine
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		state <= IDLE;
+	else
+		state <= nxt_state;
+end
+
+// edge detection on RX_line
+
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		RX_FF1 <= 1'b1;		// preset of ff
+	else
+		RX_FF1 <= RX;
+end
+
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)// preset of ff
+		RX_FF2 <= 1'b1;
+	else
+		RX_FF2 <= RX_FF1;
+	
+end
+
+
+// baud counter for sampling
+assign next_baud_cnt = ( baud_cnt == BAUD||start||shift) ? 12'h000 : baud_cnt + 1'b1;
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		baud_cnt <= 12'h000;		
+	else
+		baud_cnt <= next_baud_cnt;
+end
+
+// shift reg for received data
+assign next_shift_reg = start ? 10'h000 : 
+			shift ? {RX, shift_reg[9:1]} : shift_reg;
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		shift_reg <= 10'h000; // default
+	else
+		shift_reg <= next_shift_reg;
+end
+
+// cycle count for bits recevied
+assign next_cycle_cnt = start ? 5'h00 : 
+			shift|inc_cycle ? cycle_cnt + 1'b1 : cycle_cnt;
+always@(posedge clk or negedge rst_n) begin
+	if (~rst_n)
+		cycle_cnt <= 5'h00;
+	else
+		cycle_cnt <= next_cycle_cnt;
+end
+
+assign falling_edge_rx = ~RX_FF1 & RX_FF2;	// falling detector for RX
+assign rx_data = done ? shift_reg[8:1]: rx_data; // swaps only when done is asserted
+
+// comb logic for state transactions
+always_comb begin
+//////////////////////////
+// default //
+/////////////////////////
+start = 1'b0;
+shift = 1'b0;
+done = 1'b0;
+inc_cycle = 0;
+nxt_state = IDLE;
+
+// begin case
+case(state)
+IDLE: begin
+	if(falling_edge_rx) begin	// RX line pulled down to start transcation
+		start = 1'b1;
+		nxt_state = START;
+	end
+		
+end
+
+START: begin
+	if (baud_cnt == BAUD/2) begin	// waits for 1/2 baud before samping first RX
+		shift = 1'b1;
+		nxt_state = RECEIVING;
+	end
+	else 
+		nxt_state = START;
+end
+
+RECEIVING: begin
+	if (cycle_cnt <= 9) begin	// when cycle count is 10, then UART receving is done
+		nxt_state = RECEIVING;
+
+		if (baud_cnt == BAUD) // if baud count is reached, then sample does it 9 times
+			shift = 1'b1;
+		
+	end
+	else if (baud_cnt == BAUD/2 && cycle_cnt == 11) begin
+	done = 1'b1;			
+	nxt_state = IDLE;	
+	end
+	else if (baud_cnt == BAUD && cycle_cnt == 10) begin
+	inc_cycle = 1;
+	nxt_state = RECEIVING;
+	end
+	else
+		nxt_state = RECEIVING;
+
+end
+		
+default: 				// default state is IDLE in case when it goes to unknown state
+nxt_state = IDLE;
+
+endcase
+
+end
+
+endmodule
